42817395a762c703a963905f0148a86258c340a7
Zero <ZeroBl@hotmail.com>
2021-12-26 14:30:08 -0400

    Se ha finalizado mayor parte del proyecto, agregando JavaScript y las interfaces de las otras figuras geometricas

--- figuras.js
+++ figuras.js
@@ -1,3 +1,5 @@
+"use strict"
+
 //Codigo del Cuadrado
 
 const perimetroCuadrado = (lado) => lado * 4;
@@ -33,13 +35,43 @@ function calcularPerimetroCuadrado(){
 
 function calcularCuadrado(){
     const input = document.getElementById("InputCuadrado");
-    const AResult = document.getElementById("AreaResultado");
-    const PResult = document.getElementById("PerimetroResultado");
+    const aResult = document.getElementById("AreaResultado");
+    const pResult = document.getElementById("PerimetroResultado");
     const value = input.value;
     const area = areaCuadrado(value);
     const perimetro = perimetroCuadrado(value);
 
 
-    AResult.innerHTML = area;
-    PResult.innerHTML = perimetro;
+    aResult.innerHTML = area;
+    pResult.innerHTML = perimetro;
+}
+
+function calcularCirculo(){
+    const input = document.getElementById("InputCuadrado");
+    const aResult = document.getElementById("AreaResultado");
+    const pResult = document.getElementById("PerimetroResultado");
+    const value = input.value;
+    const area = areaCirculo(value);
+    const perimetro = perimetroCirculo(value);
+
+
+    aResult.innerHTML = area.toFixed(4);
+    pResult.innerHTML = perimetro.toFixed(4);
+}
+
+function calcularTriangulo(){
+    const base = Number(document.getElementById("InputBase").value);
+    const height = Number(document.getElementById("InputHeight").value);
+    const sideA = Number(document.getElementById("InputA").value);
+    const sideB = Number(document.getElementById("InputB").value);
+
+    const aResult = document.getElementById("AreaResultado");
+    const pResult = document.getElementById("PerimetroResultado");
+    
+    const area = areaTriangulo(base,height);
+    const perimetro = perimetroTriangulo(sideA,sideB,base);
+
+
+    aResult.innerHTML = area.toFixed(2);
+    pResult.innerHTML = perimetro.toFixed(2);
 }
